bbe9af0fdef3e75687174905f8ea77df7be6e490
root <root@1c4cc355-ae52-49a8-b7cb-f3cb5503124a>
2012-10-14 06:34:56 +0000

    resolved issue 85 - potential dns rebind attack on persistent log in cookies
    git-svn-id: file:///var/svn/fofork@70 1c4cc355-ae52-49a8-b7cb-f3cb5503124a

--- fof-main.php
+++ fof-main.php
@@ -166,14 +166,17 @@ function fof_make_salt(){
 }
 
 function fof_place_cookie($user_id){
+	global $FOF_BASE_URL;
 	$new_id = fof_make_salt();
 	$oldToken = isset($_COOKIE['token']) ? $_COOKIE['token'] : False;
 	#store to db and set cookie
 	fof_db_place_cookie($oldToken, $new_id, $user_id, $_SERVER['HTTP_USER_AGENT']);
 	//bool setcookie ( string $name [, string $value [, int $expire = 0 [, string $path [, string $domain [, bool $secure = false [, bool $httponly = false ]]]]]] )
 	//set httponly true, but https only to false (allow non-https logins, etc)
-	//possible dns rebinding problem here, as domain is not set
-	setcookie('token',$new_id, time()+60*60*24*30, '','',False,True); #30 day expiry
+	
+	//possible dns rebinding problem here, so ensure cookie can only be accessed from our domain
+	$domain = substr($FOF_BASE_URL,0,-1); //remove trailing /
+	setcookie('token',$new_id, time()+60*60*24*30, '',$domain,False,True); #30 day expiry
 }
 
 function fof_validate_cookie(){
