ed0a13c5c084a269463d7bda253c107602c9dd74
Robert Palmer <robisacommonusername@server.fake>
2013-05-09 21:42:23 +0800

    resolved issue 206, have implemented basic client side log filtering

---
+++ logs.js
@@ -0,0 +1,94 @@
+FofLogViewer = (function () {
+	my = {};
+	
+	var makeSearchFilter = function (needle, positive = true) {
+		if (positive) {
+			return function(x){
+				return x.search(needle) != -1;
+			};
+		} else {
+			return function(x){
+				return x.search(needle) == -1;
+			}
+		}
+	};
+	
+	var makeDateFilter = function(cutoffDateString, after = true) {
+		var cutoff = Date.parse(cutoffDateString);
+		return function(lineString) {
+			var lineDateString = lineString.substring(0,31);
+			var lineDate = Date.parse(lineDateString);
+			if (after) {
+				return (lineDate > cutoff);
+			} else {
+				return (lineDate < cutoff);
+			}
+		};
+	};
+	
+		
+	my.DOMobjects = [
+		{property: 'include',
+		checkbox_id: 'include_checkbox',
+		input_id: 'include',
+		filterFunction: function (haystack){
+			var needle = document.getElementById(this.input_id).value;
+			return haystack.filter(makeSearchFilter(needle, true));
+		}},
+		
+		{property: 'exclude',
+		checkbox_id: 'exclude_checkbox',
+		input_id: 'exclude',
+		filterFunction: function (haystack){
+			var needle = document.getElementById(this.input_id).value;
+			return haystack.filter(makeSearchFilter(needle, false));
+		}},
+		
+		{property: 'headtail',
+		checkbox_id: 'headtail_checkbox',
+		qty_input_id: 'head_tail_qty',
+		head_tail_input_id: 'head_or_tail',
+		filterFunction: function (haystack){
+			var numLines = document.getElementById(this.qty_input_id).value;
+			var head = (document.getElementById(this.head_tail_input_id).value == 'head');
+			if (head) {
+				return haystack.slice(0,numLines);
+			}
+			else {
+				return haystack.slice(-1*numLines);
+			}
+		}},
+		
+		{property: 'before',
+		checkbox_id: 'before_checkbox',
+		input_id: 'before_id',
+		filterFunction: function (haystack){
+			var dateString = document.getElementById(this.input_id).value;
+			return haystack.filter(makeDateFilter(dateString, false));
+		}},
+		
+		{property: 'after',
+		checkbox_id: 'after_checkbox',
+		input_id: 'after_id',
+		filterFunction: function (haystack){
+			var dateString = document.getElementById(this.input_id).value;
+			return haystack.filter(makeDateFilter(dateString, true));
+		}}
+		
+	];
+	
+	my.update = function() {
+		var newLines = my.allLines;
+		my.DOMobjects.forEach(function (obj) {
+			if (document.getElementById(obj.checkbox_id).checked) {
+				newLines = obj.filterFunction(newLines);
+			}
+		});
+		
+		//place the new lines into the text area
+		var logText = newLines.join('\n\n');
+		document.getElementById('text_area').value = logText;
+		
+	};
+	return my;
+})();
--- logs.php
+++ logs.php
@@ -17,24 +17,16 @@ if (!fof_is_admin()){
 	die('Only admin may view the logs!');
 }
 
-function decrypt_line($line){
+function decrypt_and_escape($line){
 	$IV = substr($line, 0, 22);
 	$data = substr($line, 22);
 	$aes = new Crypt_AES();
 	$aes->setKey(fof_db_log_password());
 	$aes->setIV($IV);
 	$decoded = $aes->decrypt(base64_decode($data));
-	return $decoded;
+	return htmlspecialchars($decoded, ENT_QUOTES);
 }
 
-function date_filter($line, $date, $laterThan){
-	$regex = '/^\\D*(\\d{1,2})\\s*(\\w{3})\\s*(\\d{4}).*$/';
-	preg_match($regex,$line,$match);
-	$months = array('jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec');
-	$monthi = array_search(strtolower($match[2]),$months) + 1;
-	$parsedDate = mktime(0,0,0,$monthi,$match[1],$match[3]); //hour, min, sec, month, day, year
-	return $laterThan ? ($parsedDate > $date) : ($parsedDate < $date);
-}
 //slurp all text, and split lines
 if (file_exists('fof.log')) {
 	$logLines = file('fof.log');
@@ -42,95 +34,9 @@ if (file_exists('fof.log')) {
 	$logLines = array();
 }
 
-//use head/tail.  Only look at last or first n lines
-if (isset($_POST['headtail_checkbox'])){
-	$qty = intval($_POST['headTailQty']);
-	if ($_POST['headtail'] == "head"){
-		$offset = 0;
-		$len = $qty > 0 && $qty < count($logLines) ? $qty : 0;
-	} else {
-		//tail
-		$len = $qty > 0 && $qty < count($logLines) ? $qty : 0;
-		$offset = -1*$len;
-	}
-	$logLines = array_slice($logLines, $offset, $len);
-}
-
 //decrypt everything
-$decodedLines = array_map('decrypt_line', $logLines);
-
-//check for include and exclude strings
-if (isset($_POST['include_checkbox']) || isset($_POST['exclude_checkbox'])){
-	$state = 2*(isset($_POST['include_checkbox']) ? 1 : 0) + (isset($_POST['exclude_checkbox']) ? 1 : 0);
-	if (isset($_POST['include']) && $_POST['include'] != ''){
-		$inc = $_POST['include'];
-	} else {
-		$state &= 1; //clear the "include" bit
-	}
-	if (isset($_POST['exclude']) && $_POST['exclude'] != ''){
-		$exc = $_POST['exclude'];
-	} else {
-		$state &= 2; //clear the exclude bit
-	}
-
-	switch ($state){
-		case 1: //exclude only
-		$callback = function($x) use($exc){
-			return (strpos($x,$exc) === False);
-		}; break;
-		
-		case 2: //include only
-		$callback = function ($x) use($inc){
-			return strpos($x,$inc) !== False;
-		}; break;
-		
-		case 3: //include and exclude
-		$callback = function($x) use($inc,$exc){
-			return strpos($x,$inc) !== False && strpos($x,$exc) === False;
-		}; break;
-		
-		default:
-		$callback = function($a){return True;};
-	}
-	$decodedLines = array_filter($decodedLines, $callback);
-}
-//check by date
-if (isset($_POST['before_checkbox']) || isset($_POST['after_checkbox'])){
-	$state = 2*(isset($_POST['before_checkbox']) ? 1 : 0) + (isset($_POST['after_checkbox']) ? 1 : 0);
-	if (isset($_POST['before']) && $_POST['before'] != ''){
-		$beforeParams = date_parse($_POST['before']);
-		$before = mktime(0,0,0,$beforeParams['month'],$beforeParams['day'],$beforeParams['year']);
-	} else {
-		$state &= 1; //if there's no before data, clear the before bit
-	}
-	if (isset($_POST['after']) && $_POST['after'] != ''){
-		$afterParams = date_parse($_POST['after']);
-		$after = mktime(0,0,0,$afterParams['month'],$afterParams['day'],$afterParams['year']);
-	} else {
-		$state &= 2; //if there's no after data, clear the after bit
-	}
-
-	switch ($state){
-		case 3: //before and after
-		$callback = function($line) use ($before, $after){
-			return date_filter($line, $before, False) && date_filter($line, $after, True);
-		}; break;
-		
-		case 2: //before only
-		$callback = function($line) use($before){
-			return date_filter($line, $before, False);
-		}; break;
-		
-		case 1: //after only
-		$callback = function($line) use($after){
-			return date_filter($line, $after,True);
-		}; break;
-		
-		default:
-		$callback = function($line){return True;};
-	}
-	$decodedLines = array_filter($decodedLines, $callback);
-}
+$decodedLines = array_map('decrypt_and_escape', $logLines);
+$lineArray = '["' . implode('","', $decodedLines) . '"]';
 
 if (isset($_POST['export'])){
 	//export text file
@@ -144,6 +50,7 @@ if (isset($_POST['export'])){
 	?>
 	<link rel="stylesheet" type="text/css" media="all" href="jsdatepick/jsDatePick_ltr.min.css" />
 	<script type="text/javascript" src="jsdatepick/jsDatePick.full.1.3.js"></script>
+	<script type="text/javascript" src="logs.js"></script>
 	<script type="text/javascript">
 	window.onload = function(){
 		new JsDatePick({
@@ -155,8 +62,12 @@ if (isset($_POST['export'])){
 		useMode:2,
 		target:"after_id",
 		dateFormat:"%Y-%m-%d"});
+		
+		FofLogViewer.allLines = <?php echo $lineArray; ?>;
+		FofLogViewer.update();
 	};
 	</script>
+	
 	<?php
 	//remember any previously submitted values
 	$inc = isset($_POST['include']) ? htmlspecialchars($_POST['include']) : 'search string';
@@ -172,33 +83,28 @@ if (isset($_POST['export'])){
  	?>
  	<h1>Feed on feeds log viewer</h1> <br />
  	<form method="post" action="logs.php">
- 		<input type="checkbox" name="include_checkbox" <?php echo $include_checkbox_state ? 'checked' : ''; ?>>
- 		Show lines containing the text <input type="text" value="<?php echo $inc; ?>" name="include"><br />
+ 		<input type="checkbox" name="include_checkbox" id="include_checkbox" onclick="FofLogViewer.update()" <?php echo $include_checkbox_state ? 'checked' : ''; ?>>
+ 		Show lines containing the text <input type="text" id = "include" value="<?php echo $inc; ?>" name="include"><br />
  
- 		<input type="checkbox" name="exclude_checkbox" <?php echo $exclude_checkbox_state ? 'checked' : ''; ?>>
- 		Exclude lines containing <input type="text" value="<?php echo $exc; ?>" name="exclude"><br />
+ 		<input type="checkbox" name="exclude_checkbox" id="exclude_checkbox" onclick="FofLogViewer.update()" <?php echo $exclude_checkbox_state ? 'checked' : ''; ?>>
+ 		Exclude lines containing <input type="text" id="exclude" value="<?php echo $exc; ?>" name="exclude"><br />
  
- 		<input type="checkbox" name="headtail_checkbox" <?php echo $headtail_checkbox_state ? 'checked' : ''; ?>>
- 		Search the <select name="headtail"><option value="head">First</option><option value="tail">Last</option></select><select name="headTailQty"><option value="20">20</option><option value="50">50</option><option value="100">100</option><option value="500">500</option></select> lines<br /><br />
+ 		<input type="checkbox" name="headtail_checkbox" id="headtail_checkbox" onclick="FofLogViewer.update()" <?php echo $headtail_checkbox_state ? 'checked' : ''; ?>>
+ 		Search the <select name="headtail" id="head_or_tail"><option value="head">First</option><option value="tail">Last</option></select><select name="headTailQty" id="head_tail_qty"><option value="20">20</option><option value="50">50</option><option value="100">100</option><option value="500">500</option></select> lines<br /><br />
 
-		<input type="checkbox" name="before_checkbox" <?php echo $before_checkbox_state ? 'checked' : ''; ?>>
+		<input type="checkbox" name="before_checkbox" id="before_checkbox" onclick="FofLogViewer.update()" <?php echo $before_checkbox_state ? 'checked' : ''; ?>>
 		Show results from before <input type="text" id="before_id" name="before" value="<?php echo $before;?>"><br />
 		
-		<input type="checkbox" name="after_checkbox" <?php echo $after_checkbox_state ? 'checked' : ''; ?>>
+		<input type="checkbox" name="after_checkbox" id="after_checkbox" onclick="FofLogViewer.update()" <?php echo $after_checkbox_state ? 'checked' : ''; ?>>
 		Show results from after <input type="text" id="after_id" name="after" value="<?php echo $after;?>"><br /><br />
-		
- 		<input type="checkbox" name="export" <?php echo $export_state ? 'checked' : ''; ?>>
- 		Export the log as a plain text file<br />
  
- 		<input type="submit" value="Update" name="update_btn"><br /><br />
- 	</form>
- 	<textarea rows="20" cols="100">
-	<?php
-	foreach ($decodedLines as $line){
-		$escapedLine = htmlspecialchars($line, ENT_QUOTES);
- 		echo "$escapedLine\n\n";
-	}
-	?>
+ 		<input type="submit" value="Update" name="update_btn"></form>
+ 		<form method="post" action="logs.php">
+ 		<input type="hidden" name="export" value="yes">
+ 		<input type="submit" value="Export log as text file" id="export_btn"></form>
+
+ 	<br /><br />
+ 	<textarea rows="20" cols="100" id="text_area">
 	</textarea>
 <?php
 }
