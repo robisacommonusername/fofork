ece0bb1e7b77522aa15d30d72a69f3c4f65dc20b
Robert Palmer <robisacommonusername@server.fake>
2014-01-15 11:13:33 +0800

    resolved issue 101 - added rudimentary web api

--- api.php
+++ api.php
@@ -39,7 +39,7 @@ if (array_key_exists($action, $allowed_actions)){
 	if ($action != 'login') {
 		if (!check_session()){
 			output_json(array(
-				'status' => 403,
+				'status' => 401,
 				'message' => 'Invalid session or login token. Access denied.'));
 			die();
 		}
@@ -49,12 +49,14 @@ if (array_key_exists($action, $allowed_actions)){
 	list($status,$msg,$data) = call_user_func($handler);
 	$ret['status'] = $status;
 	$ret['message'] = $msg;
-	$ret['data'] = $data;	
+	$ret['data'] = $data;
+	$ret['CSRF_hash'] = fof_compute_CSRF_challenge();	
 	
 } else {
 	$ret = array(
 	'status' => 404,
-	'message' => 'Action not found');
+	'message' => 'Action not found',
+	'CSRF_hash' => fof_compute_CSRF_challenge());
 }
 output_json($ret);
 exit();
@@ -75,10 +77,11 @@ function fof_api_login(){
 	$password = $_POST['password'];
 	$persistent = $_POST['persistent'] == 'true';
 	
-	$ret = array(403, 'Incorrect username and password');
-	if (fof_db_authenticate($username, $password)){
-		$ret = array(100,'Authentication successful');
-		if ($persistent){
+	$ret = array(401, 'Incorrect username and password');
+	$cookie_logon = fof_validate_cookie();
+	if (fof_db_authenticate($username, $password) || $cookie_logon){
+		$ret = array(200,'Authentication successful', fof_compute_CSRF_challenge());
+		if ($persistent && !$cookie_logon){
 			fof_place_cookie(fof_current_user());
 		}
 	}
@@ -89,14 +92,14 @@ function fof_api_get_feeds(){
 	$uid = fof_current_user();
 	$order = isset($_REQUEST['order']) ? $_REQUEST['order'] : 'feed_title';
 	$dirn = $_REQUEST['direction'] == 'asc' ? 'asc' : 'desc';
-	return array(100,'',fof_get_feeds($uid, $order, $dirn));
+	return array(200,'',fof_get_feeds($uid, $order, $dirn));
 }
 
 function fof_api_get_items(){
 	$uid = fof_current_user();
 	list($feed,$what,$when,$which,$how,$howmany,$order,$search) =
 		fof_safe_parse_item_constraints($_REQUEST);
-	return array(100,'',fof_get_items($uid, $feed, $what, $when, 
+	return array(200,'',fof_get_items($uid, $feed, $what, $when, 
 		$which, $howmany, $order, $search));
 	
 }
@@ -112,9 +115,55 @@ function fof_api_tag_items(){
 			$tag = $_POST['tagname'];
 			$uid = fof_current_user();
 			fof_tag_item($uid,$item_ids,$tag);
+			$ret = array(200);
 		}
 	} 
 	return $ret;
 }
 
+function fof_api_star_items(){
+	//Requires CSRF check, only use POST
+	//pass in list of items as JSON encoded array
+	$ret = array(403,'CSRF attempt detected');
+	if (fof_authenticate_CSRF_challenge($_POST['CSRF_hash'])){
+		$item_ids = json_decode($_POST['items']);
+		if (json_last_error() == JSON_ERROR_NONE){
+			$item_ids = array_map('intval', $item_ids);
+			$uid = fof_current_user();
+			fof_tag_item($uid,$item_ids,'star');
+			$ret = array(200);
+		}
+	}
+	return $ret;
+}
+
+function fof_api_mark_read(){
+	//Requires CSRF check, only use POST
+	//pass in list of items as JSON encoded array
+	$ret = array(403,'CSRF attempt detected');
+	if (fof_authenticate_CSRF_challenge($_POST['CSRF_hash'])){
+		$item_ids = json_decode($_POST['items']);
+		if (json_last_error() == JSON_ERROR_NONE){
+			$item_ids = array_map('intval', $item_ids);
+			$uid = fof_current_user();
+			fof_db_mark_read($uid, $item_ids);
+			$ret = array(200);
+		}
+	}
+	return $ret;
+}
+
+function fof_api_update_feed(){
+	$ret = array(403,'CSRF attempt detected');
+	if (fof_authenticate_CSRF_challenge($_POST['CSRF_hash'])){
+		$fid = intval($_POST['feed_id']);
+		list($count, $error) = fof_update_feed($fid);
+		if (!$error){
+			$ret = array(200,"$count new items",$count);
+		} else {
+			$ret = array(500,$error);
+		}
+	}
+	return $ret;
+}
 ?>
